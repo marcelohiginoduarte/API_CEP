021bad55d8a2bad22cc0739501831f40900bb7c3
marcelohiginoduarte <marcelihigino@gmail.com>
2024-07-22 11:25:41 -0300

    incluindo comentarios

--- cep.py
+++ cep.py
@@ -9,12 +9,14 @@ url = f'https://viacep.com.br/ws/{cep}/json/'
 response = requests.get(url)
 if response.status_code == 200:
     meuaquivo = response.json()
+    #acessando as informações da API, e mostrando os arquivos relevantes.
     print(f"Seu Cep é: {meuaquivo['cep']}")
     print(f"RUA: {meuaquivo['logradouro']}")
     print(f"Bairro: {meuaquivo['bairro']}")
     print(f"Municipio: {meuaquivo['localidade']}")
     print(f"Estado: {meuaquivo['uf']}")
 else:
+    #erro na requisição, qualquer coisa que não seja coigo 200.
     print('Falha na solicitação')
 
 
